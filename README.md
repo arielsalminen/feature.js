2f4b975d6ba24f277b97bd0e002fb33a57a7baae
Viljami Salminen <viljami.salminen@gmail.com>
2023-01-24 10:09:02 +0200

    Fix broken links

--- README.md
+++ README.md
@@ -4,11 +4,9 @@ Feature.js is a fast, simple and lightweight browser feature detection library. 
 
 With [Feature.js](http://featurejs.com/), it’s simple to build progressively enhanced experiences that use feature detection to determine if a code can be executed in the user’s browser.
 
-
-
 ## How to use
 
-First, include the script somewhere on your page — usually either in the ```<head>``` or just before the  ```</body>``` closing tag. No need to initialize or do anything else really, all the feature tests are now available for usage:
+First, include the script somewhere on your page — usually either in the `<head>` or just before the `</body>` closing tag. No need to initialize or do anything else really, all the feature tests are now available for usage:
 
 ```javascript
 if (feature.webGL) {
@@ -18,7 +16,7 @@ if (feature.webGL) {
 }
 ```
 
-If you want to add a class to the ```<html>``` element like [Modernizr](http://modernizr.com) does when something is supported, that’s simple:
+If you want to add a class to the `<html>` element like [Modernizr](http://modernizr.com) does when something is supported, that’s simple:
 
 ```javascript
 if (feature.webGL) {
@@ -38,14 +36,14 @@ Combining multiple feature tests is possible too, you can just write an if state
 
 ```javascript
 if (feature.canvas && feature.webGL) {
-  console.log("Canvas and WebGL are supported")
+  console.log("Canvas and WebGL are supported");
 }
 ```
 
 When you want to target JavaScript only towards browsers that support the features you need, you can check inside a function if the tests return false and stop further execution:
 
 ```javascript
-(function() {
+(function () {
   if (!feature.webGL || !feature.svg) {
     throw new Error("WebGL or SVG isn’t supported");
   }
@@ -53,21 +51,17 @@ When you want to target JavaScript only towards browsers that support the featur
 })();
 ```
 
-
-
 ## Gotchas
 
 There are few gotchas related to browser feature detection in general and these things are good to keep in mind when using the Feature.js library.
 
 **TOUCH:** Feature.js tries to detect if touch events are supported, but this doesn’t necessarily reflect a touch screen device. Sometimes you might get a false positive on a device that doesn’t really have touchscreen since it’s virtually [impossible](http://www.stucox.com/blog/you-cant-detect-a-touchscreen/) nowadays to detect this accurately. For 95-98% of the time this test should be correct though, but you should always keep this in mind when using this detection.
 
-**CSS 3D TRANSFORMS:** Current implementation might give false positive on some older Android stock WebKits in very rare cases. [This issue](https://github.com/viljamis/feature.js/issues/1) is currently being tracked and will be addressed in the future releases if needed.
+**CSS 3D TRANSFORMS:** Current implementation might give false positive on some older Android stock WebKits in very rare cases. [This issue](https://github.com/arielsalminen/feature.js/issues/1) is currently being tracked and will be addressed in the future releases if needed.
 
 **DEVICE MOTION & ORIENTATION:** Keep in mind that many desktop browsers support these event listeners, and will hence give a positive result even though the device might not have the [needed sensors](http://www.html5rocks.com/en/tutorials/device/orientation/).
 
 **PLACEHOLDERS:** Feature.js gives false negative for iOS 3 Safari, but since this OS has basically no users, it’s not considered an issue at the moment.
-
-
 
 ## API reference
 
@@ -76,34 +70,34 @@ There are few gotchas related to browser feature detection in general and these 
 Below you’ll find a list of all the available browser feature tests and how to call them.
 
 ```javascript
-feature.async
-feature.addEventListener
-feature.canvas
-feature.classList
-feature.cors
-feature.contextMenu
-feature.css3Dtransform
-feature.cssTransform
-feature.cssTransition
-feature.defer
-feature.deviceMotion
-feature.deviceOrientation
-feature.fetchAPI
-feature.geolocation
-feature.historyAPI
-feature.placeholder
-feature.localStorage
-feature.matchMedia
-feature.pictureElement
-feature.querySelectorAll
-feature.remUnit
-feature.serviceWorker
-feature.sizes
-feature.srcset
-feature.svg
-feature.touch
-feature.viewportUnit
-feature.webGL
+feature.async;
+feature.addEventListener;
+feature.canvas;
+feature.classList;
+feature.cors;
+feature.contextMenu;
+feature.css3Dtransform;
+feature.cssTransform;
+feature.cssTransition;
+feature.defer;
+feature.deviceMotion;
+feature.deviceOrientation;
+feature.fetchAPI;
+feature.geolocation;
+feature.historyAPI;
+feature.placeholder;
+feature.localStorage;
+feature.matchMedia;
+feature.pictureElement;
+feature.querySelectorAll;
+feature.remUnit;
+feature.serviceWorker;
+feature.sizes;
+feature.srcset;
+feature.svg;
+feature.touch;
+feature.viewportUnit;
+feature.webGL;
 ```
 
 ### CSS Feature Hooks
@@ -111,13 +105,16 @@ feature.webGL
 By running:
 
 ```javascript
-feature.testAll()
+feature.testAll();
 ```
 
 each aforementioned test will be run and its result will be added to the class names on the root `html` element on the page:
 
 ```html
-<html class=" js css3dtransform csstransform csstransition addeventlistener queryselectorall matchmedia devicemotion deviceorientation contextmenu classlist placeholder localstorage historyapi viewportunit remunit canvas svg webgl cors async defer geolocation srcset sizes pictureelement" lang="en">
+<html
+  class=" js css3dtransform csstransform csstransition addeventlistener queryselectorall matchmedia devicemotion deviceorientation contextmenu classlist placeholder localstorage historyapi viewportunit remunit canvas svg webgl cors async defer geolocation srcset sizes pictureelement"
+  lang="en"
+></html>
 ```
 
 ### Extending
@@ -134,7 +131,7 @@ This way you can add your own tests to the library, the result of which will als
 <script src="path/to/feature.js"></script>
 
 <script>
-  feature.extend('foo', function () {
+  feature.extend("foo", function () {
     return true;
   });
 
@@ -144,40 +141,35 @@ This way you can add your own tests to the library, the result of which will als
 
 This would add the `foo` class name to the `html` document element as well.
 
-Use this to write your own tests, customize existing tests, or as a stop-gap measure to fix a bug in a test between releases of the library. Please share the love, if you find yourself in the latter example, by [submitting an issue](https://github.com/viljamis/feature.js/issues) for us to fix in an upcoming release.
+Use this to write your own tests, customize existing tests, or as a stop-gap measure to fix a bug in a test between releases of the library. Please share the love, if you find yourself in the latter example, by [submitting an issue](https://github.com/arielsalminen/feature.js/issues) for us to fix in an upcoming release.
 
 #### Utilities
 
 `feature.extend` will graciously expose the utility methods to your callback. These utilities are used internally within the library and will help unify your added tests:
 
 ```javascript
-feature.extend('foo', function (util) {
+feature.extend("foo", function (util) {
   // Simple create element method
-  util.create('image'); // returns HTMLElement
+  util.create("image"); // returns HTMLElement
 
   // Test if it's an old device that we want to filter out
   util.old; // returns Boolean
 
   // Function that takes a standard CSS property name as a parameter and returns
   // its prefixed version valid for the current browser it runs in
-  util.pfx('transition'); // Returns falsey if unsupported, truthy if supported
+  util.pfx("transition"); // Returns falsey if unsupported, truthy if supported
 });
 ```
 
-
 ## Live demo
 
-* Official site with live tests: [http://featurejs.com](http://featurejs.com)
-* Test with actual features: [http://featurejs.com/test](http://featurejs.com/test)
-
-
+- Official site with live tests: [http://featurejs.com](http://featurejs.com)
+- Test with actual features: [http://featurejs.com/test](http://featurejs.com/test)
 
 ## Download
 
-* [Download the latest release](https://github.com/viljamis/feature.js/archive/master.zip)
-* Install by running `npm install feature.js`
-
-
+- [Download the latest release](https://github.com/arielsalminen/feature.js/archive/master.zip)
+- Install by running `npm install feature.js`
 
 ## Contributing
 
@@ -212,33 +204,29 @@ $ npm run test
 
 View the full changelog [CHANGELOG.md](here).
 
-
-
 ## Tested on the following platforms
 
-* iOS 3.0+
-* Android 1.6+
-* Windows Phone 7.0+
-* Blackberry 3.0+
-* Blackberry Tablet 1.0+
-* Jolla 1.0+
-* Kindle 3.3+
-* Maemo 5.0+
-* Meego 1.2+
-* Symbian 3
-* Symbian Belle
-* Symbian S40 Asha
-* webOS 2.0+
-* Windows XP+ (IE6 and up)
-* Mac OS X
-
-
+- iOS 3.0+
+- Android 1.6+
+- Windows Phone 7.0+
+- Blackberry 3.0+
+- Blackberry Tablet 1.0+
+- Jolla 1.0+
+- Kindle 3.3+
+- Maemo 5.0+
+- Meego 1.2+
+- Symbian 3
+- Symbian Belle
+- Symbian S40 Asha
+- webOS 2.0+
+- Windows XP+ (IE6 and up)
+- Mac OS X
 
 ## License
 
 The MIT License (MIT)
 
-Copyright (c) 2016 [Viljami Salminen](https://viljamis.com/)
+Copyright (c) 2023 [Ariel Salminen](https://arie.ls/)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- feature.js
+++ feature.js
@@ -17,21 +17,21 @@
  *   console.log('webGL supported!');
  * }
  *
- * Author: @viljamis, https://viljamis.com
+ * Author: @ariel, https://arie.ls
  */
 
 /* globals DocumentTouch */
 /* eslint no-extra-semi: 0 */
-;(function(root, document, factory) {
-  if (typeof define === 'function' && define.amd) {
+(function (root, document, factory) {
+  if (typeof define === "function" && define.amd) {
     // AMD. Register as an anonymous module.
     define([], factory);
   } else {
     // Browser globals (root is window)
     root.feature = factory();
   }
-}(typeof self !== 'undefined' ? self : this, document, function() {
-  'use strict';
+})(typeof self !== "undefined" ? self : this, document, function () {
+  "use strict";
 
   // For minification only
   var docEl = document.documentElement;
@@ -39,38 +39,36 @@
   // Protect against undefined window.navigator objects
   var nav = window.navigator || {};
 
-
   /**
    * Utilities
    */
   var util = {
-
     /**
      * Simple create element method
      */
-    create: function(el) {
+    create: function (el) {
       return document.createElement(el);
     },
 
     /**
      * Test if it's an old device that we want to filter out
      */
-    old: !!(/(Android\s(1\.|2\.))|(Silk\/1\.)/i.test(nav.userAgent)),
+    old: !!/(Android\s(1\.|2\.))|(Silk\/1\.)/i.test(nav.userAgent),
 
     /**
      * Function that takes a standard CSS property name as a parameter and
      * returns it's prefixed version valid for current browser it runs in
      */
-    pfx: (function() {
-      var style = document.createElement('dummy').style;
-      var prefixes = ['Webkit', 'Moz', 'O', 'ms'];
+    pfx: (function () {
+      var style = document.createElement("dummy").style;
+      var prefixes = ["Webkit", "Moz", "O", "ms"];
       var memory = {};
 
-      return function(prop) {
-        if (typeof memory[prop] === 'undefined') {
+      return function (prop) {
+        if (typeof memory[prop] === "undefined") {
           var ucProp = prop.charAt(0).toUpperCase() + prop.substr(1);
-          var prefixedProps = prefixes.join(ucProp + ' ');
-          var props = (prop + ' ' + prefixedProps + ucProp).split(' ');
+          var prefixedProps = prefixes.join(ucProp + " ");
+          var props = (prop + " " + prefixedProps + ucProp).split(" ");
 
           memory[prop] = null;
 
@@ -84,9 +82,7 @@
         return memory[prop];
       };
     })(),
-
   };
-
 
   /**
    * The Feature.js object
@@ -94,20 +90,20 @@
 
   var Feature = {
     // Test if CSS 3D transforms are supported
-    css3Dtransform: (function() {
-      var test = (!util.old && util.pfx('perspective') !== null);
+    css3Dtransform: (function () {
+      var test = !util.old && util.pfx("perspective") !== null;
       return !!test;
     })(),
 
     // Test if CSS transforms are supported
-    cssTransform: (function() {
-      var test = (!util.old && util.pfx('transformOrigin') !== null);
+    cssTransform: (function () {
+      var test = !util.old && util.pfx("transformOrigin") !== null;
       return !!test;
     })(),
 
     // Test if CSS transitions are supported
-    cssTransition: (function() {
-      var test = util.pfx('transition') !== null;
+    cssTransition: (function () {
+      var test = util.pfx("transition") !== null;
       return !!test;
     })(),
 
@@ -121,25 +117,25 @@
     matchMedia: !!window.matchMedia,
 
     // Test if Device Motion is supported
-    deviceMotion: ('DeviceMotionEvent' in window),
+    deviceMotion: "DeviceMotionEvent" in window,
 
     // Test if Device Orientation is supported
-    deviceOrientation: ('DeviceOrientationEvent' in window),
+    deviceOrientation: "DeviceOrientationEvent" in window,
 
     // Test if Context Menu is supported
-    contextMenu: ('contextMenu' in docEl && 'HTMLMenuItemElement' in window),
+    contextMenu: "contextMenu" in docEl && "HTMLMenuItemElement" in window,
 
     // Test if classList API is supported
-    classList: ('classList' in docEl),
+    classList: "classList" in docEl,
 
     // Test if placeholder attribute is supported
-    placeholder: ('placeholder' in util.create('input')),
+    placeholder: "placeholder" in util.create("input"),
 
     // Test if localStorage is supported
-    localStorage: (function() {
+    localStorage: (function () {
       try {
-        window.localStorage.setItem('featurejs-test', 'foobar');
-        window.localStorage.removeItem('featurejs-test');
+        window.localStorage.setItem("featurejs-test", "foobar");
+        window.localStorage.removeItem("featurejs-test");
         return true;
       } catch (err) {
         // no content in the cache means it couldn't be added to at all (old
@@ -149,110 +145,114 @@
     })(),
 
     // Test if History API is supported
-    historyAPI: (window.history && 'pushState' in window.history),
+    historyAPI: window.history && "pushState" in window.history,
 
     // Test if ServiceWorkers are supported
-    serviceWorker: ('serviceWorker' in nav),
+    serviceWorker: "serviceWorker" in nav,
 
     // Test if viewport units are supported
-    viewportUnit: (function(el) {
+    viewportUnit: (function (el) {
       try {
-        el.style.width = '1vw';
-        var test = el.style.width !== '';
+        el.style.width = "1vw";
+        var test = el.style.width !== "";
         return !!test;
       } catch (err) {
         return false;
       }
-    })(util.create('dummy')),
+    })(util.create("dummy")),
 
     // Test if REM units are supported
-    remUnit: (function(el) {
+    remUnit: (function (el) {
       try {
-        el.style.width = '1rem';
-        var test = el.style.width !== '';
+        el.style.width = "1rem";
+        var test = el.style.width !== "";
         return !!test;
       } catch (err) {
         return false;
       }
-    })(util.create('dummy')),
+    })(util.create("dummy")),
 
     // Test if Canvas is supported
-    canvas: (function(el) {
-      return !!(el.getContext && el.getContext('2d'));
-    })(util.create('canvas')),
+    canvas: (function (el) {
+      return !!(el.getContext && el.getContext("2d"));
+    })(util.create("canvas")),
 
     // Test if SVG is supported
-    svg: !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect,
+    svg:
+      !!document.createElementNS &&
+      !!document.createElementNS("http://www.w3.org/2000/svg", "svg")
+        .createSVGRect,
 
     // Test if WebGL is supported
-    webGL: (function(el) {
+    webGL: (function (el) {
       try {
-        return !!(window.WebGLRenderingContext &&
-          (el.getContext('webgl') || el.getContext('experimental-webgl')));
+        return !!(
+          window.WebGLRenderingContext &&
+          (el.getContext("webgl") || el.getContext("experimental-webgl"))
+        );
       } catch (err) {
         return false;
       }
-    })(util.create('canvas')),
+    })(util.create("canvas")),
 
     // Test if cors is supported
-    cors: ('XMLHttpRequest' in window &&
-      'withCredentials' in new XMLHttpRequest()),
+    cors:
+      "XMLHttpRequest" in window && "withCredentials" in new XMLHttpRequest(),
 
     // Tests if touch events are supported, but doesn't necessarily reflect a
     // touchscreen device
-    touch: !!(('ontouchstart' in window) ||
-      nav.msPointerEnabled &&
-      window.MSGesture ||
-      window.DocumentTouch &&
-      document instanceof DocumentTouch),
+    touch: !!(
+      "ontouchstart" in window ||
+      (nav.msPointerEnabled && window.MSGesture) ||
+      (window.DocumentTouch && document instanceof DocumentTouch)
+    ),
 
     // Test if async attribute is supported
-    async: ('async' in util.create('script')),
+    async: "async" in util.create("script"),
 
     // Test if defer attribute is supported
-    defer: ('defer' in util.create('script')),
+    defer: "defer" in util.create("script"),
 
     // Test if Geolocation is supported
-    geolocation: ('geolocation' in nav),
+    geolocation: "geolocation" in nav,
 
     // Test if img srcset attribute is supported
-    srcset: ('srcset' in util.create('img')),
+    srcset: "srcset" in util.create("img"),
 
     // Test if img sizes attribute is supported
-    sizes: ('sizes' in util.create('img')),
+    sizes: "sizes" in util.create("img"),
 
     // Test if Picture element is supported
-    pictureElement: ('HTMLPictureElement' in window),
+    pictureElement: "HTMLPictureElement" in window,
 
     // Test if Fetch is supported
-    fetch: ('fetch' in window),
+    fetch: "fetch" in window,
 
     // Run all the tests and add supported classes
-    testAll: function() {
-      var classes = ' js';
+    testAll: function () {
+      var classes = " js";
 
       for (var test in this) {
-        if (test !== 'testAll' && test !== 'extend' && this[test]) {
-          classes += ' ' + test;
+        if (test !== "testAll" && test !== "extend" && this[test]) {
+          classes += " " + test;
         }
       }
 
       docEl.className += classes.toLowerCase();
     },
 
-    extend: function(name, callback) {
-      if (typeof callback !== 'function') {
-        throw new TypeError('Feature.extend: `callback` is not a Function');
+    extend: function (name, callback) {
+      if (typeof callback !== "function") {
+        throw new TypeError("Feature.extend: `callback` is not a Function");
       }
 
       this[name] = !!callback(util);
       return this;
     },
-
   };
 
   /**
    * Expose a public-facing API
    */
   return Feature;
-}));
+});
